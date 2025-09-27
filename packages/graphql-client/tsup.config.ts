8c2aec49cae859885526ab639508b315389ed512
AimotoRyosuke <r.aimoto97vg@gmail.com>
2025-09-27 20:39:58 +0900

    change dts to true

--- packages/graphql-client/tsup.config.ts
+++ packages/graphql-client/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: false, // 一時的にdtsを無効化
+  dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
